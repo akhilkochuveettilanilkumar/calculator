dea3f182bfa9471bacc39e0b0ea6bfffea533faa
Akhil K A <akhilka@Akhils-MacBook-Air.local>
2025-08-10 14:06:59 +1000

    Removed code

--- substraction.py
+++ substraction.py
@@ -2,11 +2,4 @@
 
 # Get input from the user
 num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-# Perform addition
-sum_result = num1 - num2
-
-# Display the result
-print("The sum is:", sum_result)
 
